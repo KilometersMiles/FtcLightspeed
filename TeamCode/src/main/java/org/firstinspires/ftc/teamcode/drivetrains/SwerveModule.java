45f7a5e23c17f0f113288433707f6317e7ea7e4a
Miles Higginson <114202378+KilometersMiles@users.noreply.github.com>
2026-03-09 13:17:33 -0600

    update swerve module - not ready yet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrains/SwerveModule.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivetrains/SwerveModule.java
@@ -2,13 +2,21 @@ package org.firstinspires.ftc.teamcode.drivetrains;
 
 public class SwerveModule {
     private DcMotorEx motor;
-    private CRServo servo;
+    private RTPAxon axon;
 
     public SwerveModule() {
         //init hardware
     }
 
     public void setSpeedAndDirection(double speed, double angle) {
-        //do something
+        double angleError = Math.abs(angle - axon.getCurrentAngleRadians());
+        if (angleError > Math.PI/2) {
+            angle -= Math.PI;
+            speed *= -1;
+        }        
+
+        axon.setTarget();
+        //make sure speed is normalized
+        motor.setPower(speed);
     }
 }
